0d0efccf18e7730909b7498cc58c2351be280c7a
Claudio Espinoza <claudioyugen@gmail.com>
2024-07-31 19:09:52 -0400

    Feat: Agrega recursos visual

---
+++ lib/core/configs/themes/app_colors.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  //--|| Colores de sistema ||--//
+  static const background = Color(0xfff2f2f2);
+  static const font = Color(0xff222222);
+  static const disableFont = Color(0xff757575);
+  static const input = Color(0xffEAE8F2);
+  static const iconGray = Color(0xff717171);
+  static const icon = Color(0xff222222);
+  static const alertGreen = Color(0xff80FF79);
+  static const alerRed = Color(0xfff69494);
+
+  //--|| Colores por tipos ||--//
+  static const steel = Color(0xffb0d0db); // Acero
+  static const water = Color(0xff57aaf2); // Agua
+  static const incest = Color(0xffbecc53); // Insecto
+  static const dragon = Color(0xff6777ee); // Dragon
+  static const thunderbolt = Color(0xffffd446); // Rayo
+  static const ghost = Color(0xff885d88); // Fantasma
+  static const fire = Color(0xffee4e4f); // Fuego
+  static const fairy = Color(0xfffa96fa); // Hada
+  static const ice = Color(0xff4edbff); // Hielo
+  static const struggle = Color(0xffff8b16); // Lucha
+  static const normal = Color(0xff9fA19f); // Normal
+  static const plant = Color(0xff78e160); // Planta
+  static const psychic = Color(0xfff95f91); // Psíquico
+  static const rock = Color(0xffc8c298); // Roca
+  static const sinister = Color(0xff2c2625); // Siniestro
+  static const earth = Color(0xffbe6d30); // Tierra
+  static const poison = Color(0xffa84fed); // Veneno
+  static const flying = Color(0xff6777EE); // Volador
+}
