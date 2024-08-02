6e919c34392858465334f6ff9ac2c3be1019975e
Claudio Espinoza <claudioyugen@gmail.com>
2024-08-02 17:33:26 -0400

    Refactor: Reorganización de las paginas

---
+++ lib/core/configs/themes/app_themes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_demo/core/configs/themes/app_colors.dart';
+
+class AppThemes {
+  /*
+  static const lightTheme = ThemeData(
+    dialogBackgroundColor: AppColors.background,
+    scaffoldBackgroundColor: AppColors.background
+
+  )
+  */
+}
---
+++ lib/presentation/page/landing/landing.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_demo/core/configs/themes/app_colors.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Container(
+        color: AppColors.alerRed,
+        width: double.infinity,
+        height: 200,
+        child: const Column(
+          children: [
+            Text('Pokedex',
+                style: TextStyle(fontSize: 32, color: AppColors.font, fontFamily: )),
+          ],
+        ),
+      ),
+    );
+  }
+}
