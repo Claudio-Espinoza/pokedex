babdc3f8852e3429a81001c255ca0949c8bec22a
Claudio Espinoza <claudioyugen@gmail.com>
2024-08-04 14:29:26 -0400

    Fix: Se aplicaron unidades relativas
    En la seccion principal de la landing se aplicaron unidades relativas

--- lib/presentation/landing/widget/presentation_article.dart
+++ lib/presentation/landing/widget/presentation_article.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:pokedex_demo/core/configs/themes/app_colors.dart';
 import 'package:pokedex_demo/core/configs/themes/app_layer.dart';
 
@@ -20,44 +19,34 @@ class PresentationArticle extends StatelessWidget {
           height: double.maxFinite,
           decoration: BoxDecoration(
             color: AppColors.red,
-            borderRadius: BorderRadius.circular(AppLayer.borderRadius),
+            borderRadius: BorderRadius.circular(20.0),
           ),
           child: Padding(
             padding: const EdgeInsets.only(
                 top: AppLayer.marginVertical,
                 right: AppLayer.marginHorizontal - 10,
                 bottom: AppLayer.marginVertical),
-            child: Stack(
-              children: [
-                const Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.end,
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                double topPokeball = constraints.maxHeight * 0.05;
+                double endPokeball = constraints.maxWidth * 0.6;
+                double topCharizard = constraints.maxHeight * -0.05;
+                double leftCharizard = constraints.maxWidth * 0.02;
+
+                double pokeballSize = constraints.maxWidth * 0.8;
+                double charizardSize = constraints.maxWidth * 1.02;
+                return Stack(
+                  fit: StackFit.expand,
                   children: [
-                    Text(
-                      'Pokémon',
-                      style: TextStyle(
-                        fontSize: 34,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        shadows: [
-                          Shadow(
-                            offset: Offset(2.0, 2.0),
-                            blurRadius: 3.0,
-                            color: Color.fromARGB(50, 0, 0, 0),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 5),
-                    Row(
+                    const Column(
                       mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
                         Text(
-                          'Se cuenta con 151 pokemones de la \n'
-                          'primera generación',
-                          textAlign: TextAlign.right,
+                          'Pokémon',
                           style: TextStyle(
-                            fontSize: 18,
+                            fontSize: 34,
+                            fontWeight: FontWeight.bold,
                             color: Colors.white,
                             shadows: [
                               Shadow(
@@ -68,33 +57,54 @@ class PresentationArticle extends StatelessWidget {
                             ],
                           ),
                         ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              'Se cuenta con 151 pokemones de la \n'
+                              'primera generación',
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                                shadows: [
+                                  Shadow(
+                                    offset: Offset(2.0, 2.0),
+                                    blurRadius: 3.0,
+                                    color: Color.fromARGB(50, 0, 0, 0),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ],
                     ),
-                  ],
-                ),
-                PositionedDirectional(
-                  top: 45,
-                  end: 190,
-                  child: SizedBox(
-                    width: 220,
-                    height: 220,
-                    child: Opacity(
-                      opacity: 0.2,
-                      child: Image.asset(
-                        'assets/icon/pokeball.png',
-                        fit: BoxFit.cover,
+                    PositionedDirectional(
+                      top: topPokeball,
+                      end: endPokeball,
+                      child: SizedBox(
+                        width: pokeballSize,
+                        height: pokeballSize,
+                        child: Opacity(
+                          opacity: 0.2,
+                          child: Image.asset(
+                            'assets/icon/pokeball.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                Positioned(
-                  width: 320,
-                  height: 320,
-                  top: -20,
-                  left: 0,
-                  child: Image.asset('assets/icon/charizard.png'),
-                ),
-              ],
+                    Positioned(
+                      width: charizardSize,
+                      height: charizardSize,
+                      top: topCharizard,
+                      left: leftCharizard,
+                      child: Image.asset('assets/icon/charizard.png'),
+                    ),
+                  ],
+                );
+              },
             ),
           ),
         ),
