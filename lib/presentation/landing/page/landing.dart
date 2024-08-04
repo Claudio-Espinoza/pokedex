27ea6109b0bde690f906e666351a9ea0fe912ce6
Claudio Espinoza <claudioyugen@gmail.com>
2024-08-04 15:21:27 -0400

    Fix: Simplifica el nombre de landing page

--- lib/presentation/landing/page/landing.dart
+++ lib/presentation/landing/page/landing.dart
@@ -1,33 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex_demo/presentation/landing/widget/header_landing.dart';
-import 'package:pokedex_demo/presentation/landing/widget/presentation_article.dart';
+import 'package:pokedex_demo/presentation/landing/widget/option_landing.dart';
+import 'package:pokedex_demo/presentation/landing/widget/presentation_landing.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SizedBox(
         width: double.infinity,
         height: double.maxFinite,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Flexible(
+            Flexible(
               flex: 1,
               child: HeaderLanding(),
             ),
-            const Flexible(
+            Flexible(
               flex: 3,
               child: PresentationArticle(),
             ),
             Flexible(
               flex: 2,
-              child: Container(
-                color: Colors.blue,
-                child: const Center(child: Text('Component 3')),
-              ),
+              child: OptionLanding(),
             ),
           ],
         ),
