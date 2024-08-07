596bd4829d10f7b339e797075ea5767b0cd56f2a
Daniel Alejandro <d.toro05@ufromail.cl>
2024-08-07 04:15:17 -0400

    Add<Pokedex>: Se agrego funcionalidad a la flecha para que redirija a la LandigPage

--- lib/presentation/pokedex/page/pokedex.dart
+++ lib/presentation/pokedex/page/pokedex.dart
@@ -2,73 +2,100 @@ import 'package:flutter/material.dart';
 import 'package:pokedex_demo/core/configs/themes/app_colors.dart';
 import 'package:pokedex_demo/core/configs/themes/app_layer.dart';
 import 'package:pokedex_demo/common/widget/search_bard.dart';
+import 'package:pokedex_demo/presentation/landing/page/landing.dart';
 
-
-class Pokedex extends StatelessWidget {
+class Pokedex extends StatefulWidget {
   const Pokedex({super.key});
 
   @override
+  _PokedexState createState() => _PokedexState();
+}
+
+class _PokedexState extends State<Pokedex> {
+  late TextEditingController searchController;
+
+  @override
+  void initState() {
+    super.initState();
+    searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-  final TextEditingController searchController = TextEditingController();
     return Scaffold(
       backgroundColor: AppColors.background,
       body: Padding(
-        padding: const EdgeInsets.only(top: 80, right: AppLayer.marginHorizontal, left: AppLayer.marginHorizontal ),
+        padding: const EdgeInsets.only(
+          top: 80,
+          right: AppLayer.marginHorizontal,
+          left: AppLayer.marginHorizontal,
+        ),
         child: Column(
-        
-          
           children: [
-        
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Column(
-                    children: [
-                      Icon(Icons.arrow_back, size: 30,),
-                    ],
-                  ),
-                  Title(
-                    color: Colors.black,
-                    child: const Text("Pokédex",style: 
-                    TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    IconButton(
+                      onPressed: () => redirectToPage(const LandingPage()),
+                      icon: const Icon(Icons.arrow_back)),
+                  ],
+                ),
+                Title(
+                  color: Colors.black,
+                  child: const Text(
+                    "Pokédex",
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
                     ),
-                      ),
-                Container(width: 30,)], // Este container me permite usar el spaceBetween para tener el Titulo al centro y la flecha a la izquierda de la manera mas limpia que encontre
-              ),    
-              Padding(
-              padding: const EdgeInsets.only(bottom: 20, top: 20),
-              child: SearchInput(controller: searchController)),
-              Container(
-                height: 70,
-
-                child:
-                  ListView(
-                    
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
-                      Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
-                      Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
-                      Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
-                      Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
-                      Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
-                      Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
-                      Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
-                      Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
-                      Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
-
-                    ],
                   ),
-
-              )
+                ),
+                Container(width: 30),
+              ], // Este container me permite usar el spaceBetween para tener el Titulo al centro y la flecha a la izquierda de la manera mas limpia que encontre
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20, top: 20),
+              child: SearchInput(controller: searchController),
+            ),
+            Container(
+              height: 70,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
+                  Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
+                  Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
+                  Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
+                  Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
+                  Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
+                  Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
+                  Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
+                  Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
+                  Container(width: 70, height: 70, color: Colors.red, margin: const EdgeInsets.only(right: 10)),
                 ],
               ),
             ),
-           
-    
+          ],
+        ),
+      ),
+    );
+  }
+   Future<void> redirectToPage(Widget page) async {
+    if (!mounted) return;
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (BuildContext context) => page,
+      ),
     );
   }
 }
