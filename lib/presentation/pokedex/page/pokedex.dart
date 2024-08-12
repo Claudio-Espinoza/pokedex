c792e78ba95a1b03c7a39b20589fa699c3d5abdc
Daniel Alejandro <d.toro05@ufromail.cl>
2024-08-12 00:51:59 -0400

    Update <podedex>: Actualizacion de origen de pokemon de pokedex y correccion de color

--- lib/presentation/pokedex/page/pokedex.dart
+++ lib/presentation/pokedex/page/pokedex.dart
@@ -1,8 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:pokedex_demo/common/enum/type_colors.dart';
 import 'package:pokedex_demo/common/widget/custom_buton.dart';
 import 'package:pokedex_demo/core/configs/themes/app_colors.dart';
 import 'package:pokedex_demo/core/configs/themes/app_layer.dart';
 import 'package:pokedex_demo/common/widget/search_bard.dart';
+import 'package:pokedex_demo/domain/model/pokemon.dart';
+import 'package:pokedex_demo/domain/service/pokemon_service.dart';
 import 'package:pokedex_demo/presentation/landing/page/landing.dart';
 import 'package:pokedex_demo/common/enum/types.dart';
 
@@ -13,6 +18,8 @@ class Pokedex extends StatefulWidget {
   _PokedexState createState() => _PokedexState();
 }
 
+/*
+
 class Pokemon {
   var name;
   var tipo = List.empty(growable: true);
@@ -22,22 +29,18 @@ class Pokemon {
     required this.tipo,
   });
 }
-
+*/
 class _PokedexState extends State<Pokedex> {
   late TextEditingController searchController;
-  List<Pokemon> pokemons = [
-    Pokemon(name: "Bulbasaur", tipo: ["Grass", "Poison"]),
-    Pokemon(name: "Squirtle", tipo: ["Water"]),
-    Pokemon(name: "Venusaur", tipo: ["Grass", "Poison"])
-  ];
+  Future<List<Pokemon>> pokemons = PokemonService().getAllPokemon();
   List<Pokemon> seleccionados = <Pokemon>[];
+
   Set<Type> tiposFilter = <Type>{};
 
   @override
   void initState() {
     super.initState();
     searchController = TextEditingController();
-    seleccionados = pokemons;
   }
 
   @override
@@ -46,16 +49,14 @@ class _PokedexState extends State<Pokedex> {
     super.dispose();
   }
 
-  void _filterPokemons() {
-    setState(() {
-      if (tiposFilter.isEmpty) {
-        seleccionados = pokemons;
-      } else {
-        seleccionados = pokemons.where((pokemon) {
-          return tiposFilter.any((tipo) => pokemon.tipo.contains(tipo.type));
-        }).toList();
-      }
-    });
+  List<Pokemon> _filterPokemons(List<Pokemon> pokemons) {
+    if (tiposFilter.isEmpty) {
+      return pokemons;
+    } else {
+      return pokemons.where((pokemon) {
+        return tiposFilter.any((tipo) => pokemon.type.contains(tipo.type));
+      }).toList();
+    }
   }
 
   @override
@@ -100,63 +101,79 @@ class _PokedexState extends State<Pokedex> {
                   bottom: AppLayer.paddingButton, top: AppLayer.paddingTop),
               child: SearchInput(controller: searchController),
             ),
-           SizedBox(
-            height: 55,
-            child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: Type.values.map((Type tipo) {
-              final isSelected = tiposFilter.contains(tipo);
-              return Container(
-                width: 55,
-                height: 55,
-                margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                child: InkWell(
-                  onTap: () {
-                    setState(() {
-                      if (isSelected) {
-                        tiposFilter.remove(tipo);
-                      } else {
-                        tiposFilter.add(tipo);
-                      }
-                      _filterPokemons();
-                    });
-                  },
-                  child: ColorFiltered(
-                    colorFilter: ColorFilter.mode(
-                      isSelected ? const Color.fromARGB(41, 0, 0, 0): const Color.fromARGB(0, 0, 0, 0),
-                      BlendMode.multiply,
+            SizedBox(
+              height: 55,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: Type.values.map((Type tipo) {
+                  final isSelected = tiposFilter.contains(tipo);
+                  return Container(
+                    width: 55,
+                    height: 55,
+                    margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          if (isSelected) {
+                            tiposFilter.remove(tipo);
+                          } else {
+                            tiposFilter.add(tipo);
+                          }
+                        });
+                      },
+                      child: ColorFiltered(
+                        colorFilter: ColorFilter.mode(
+                          isSelected
+                              ? const Color.fromARGB(80, 0, 0, 0)
+                              : const Color.fromARGB(0, 0, 0, 0),
+                          BlendMode.multiply,
+                        ),
+                        child: Image.asset(tipo.path),
+                      ),
                     ),
-                    child: Image.asset(tipo.path),
-                  ),
-                ),
-              );
-            }).toList(),
+                  );
+                }).toList(),
+              ),
             ),
-            )
-
-            ,
             const Padding(
               padding: EdgeInsets.only(top: 30),
               child: Row(
                 children: [
-                  Text("Pokemon encontrados:", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w300)),
+                  Text("Pokemon encontrados:",
+                      style: TextStyle(
+                          fontSize: 22, fontWeight: FontWeight.w300)),
                 ],
               ),
             ),
             Expanded(
-              child: ListView.builder(
-                itemCount: seleccionados.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding:
-                        const EdgeInsets.only(bottom: AppLayer.marginInternal),
-                    child: CustomButton(
-                        text: seleccionados[index].name +
-                            "; " +
-                            seleccionados[index].tipo.join(', '),
-                        onPressed: () => redirectToPage(const LandingPage()),
-                        backgroundColor: AppColors.green),
-                  );
+              child: FutureBuilder<List<Pokemon>>(
+                future: pokemons,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text("Error: ${snapshot.error}"));
+                  } else if (snapshot.hasData) {
+                    List<Pokemon> filteredPokemons =
+                        _filterPokemons(snapshot.data!);
+                    return ListView.builder(
+                      itemCount: filteredPokemons.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(
+                              bottom: AppLayer.marginInternal),
+                          child: CustomButton(
+                              text:
+                                  "${filteredPokemons[index].name}; ${filteredPokemons[index].type.join(', ')}",
+                              onPressed: () =>
+                                  redirectToPage(const LandingPage()),
+                              backgroundColor: TypeColor.pokemonTypeColors[filteredPokemons[index].type[0].toString()]
+                        ));
+                      },
+                    );
+                  } else {
+                    return const Center(child: Text("No Pokémon found"));
+                  }
                 },
               ),
             ),
