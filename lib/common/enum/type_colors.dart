450885f4e46d433bc5d52e19f318c1d7508d3801
Daniel Alejandro <d.toro05@ufromail.cl>
2024-08-12 00:51:00 -0400

    Create <TypeColor>: Creacion de clase con hashmap <tipo, color>

---
+++ lib/common/enum/type_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_demo/core/configs/themes/app_colors.dart';
+
+class TypeColor{
+
+  static final  Map<String, Color> pokemonTypeColors = {
+    'Normal': AppColors.normal,      // Normal
+    'Fire': AppColors.fire,      // Fuego
+    'Water': AppColors.water ,        // Agua
+    'Grass': AppColors.plant,     // Planta
+    'Electric': AppColors.thunderbolt,   // Rayo
+    'Ice': AppColors.ice,      // Hielo
+    'Fighting': AppColors.struggle,       // Lucha
+    'Poison': AppColors.poison,      // Veneno
+    'Ground': AppColors.earth,        // Tierra
+    'Flying': AppColors.flying,   // Volador
+    'Psychic': AppColors.psychic,     // Psiquico
+    'Bug': AppColors.incest,      // Incecto
+    'Rock': AppColors.rock,    // Roca
+    'Ghost': AppColors.ghost,       // Fantasma
+    'Dragon': AppColors.dragon,       // Dragon
+  };
+
+
+
+}
