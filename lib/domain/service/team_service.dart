56f6505448b06119070b977ecdcad1ac5134e4f5
Claudio Espinoza <claudioyugen@gmail.com>
2024-08-14 17:16:23 -0400

    refactor: agrega servicios de guardado

--- lib/domain/service/team_service.dart
+++ lib/domain/service/team_service.dart
@@ -3,6 +3,81 @@ import 'package:hive/hive.dart';
 import 'package:pokedex_demo/domain/model/team.dart';
 
 class TeamService {
+  Future<void> addPokemonToTeam(int teamIndex, PokemonTeam pokemon) async {
+    try {
+      Box<Team> teamBox = await Hive.openBox<Team>('teamBox');
+      Team? team = teamBox.getAt(teamIndex);
+      if (team != null) {
+        team.pokemonTeam.add(pokemon);
+        team.save();
+      } else {
+        if (kDebugMode) {
+          print('Team not found at index $teamIndex');
+        }
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error adding Pokemon to Team: $e');
+      }
+    }
+  }
+
+  Future<int> getTeamIndexByName(String teamName) async {
+    try {
+      Box<Team> teamBox = await Hive.openBox<Team>('teamBox');
+      for (int i = 0; i < teamBox.length; i++) {
+        Team? team = teamBox.getAt(i);
+        if (team != null && team.nameTeam == teamName) {
+          return i;
+        }
+      }
+      return -1;
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error getting team index by name: $e');
+      }
+      return -1;
+    }
+  }
+
+  Future<void> saveTeam(Team team) async {
+    try {
+      Box<Team> teamBox = await Hive.openBox<Team>('teamBox');
+      await teamBox.add(team);
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error saving Team data: $e');
+      }
+    }
+  }
+
+  Future<void> printAllTeam(List<Team> teamList) async {
+    try {
+      for (var team in teamList) {
+        if (kDebugMode) {
+          print('Name: ${team.nameTeam}');
+          print('Number: ${team.contPokemon}');
+          print('Height: ${team.descriptionTeam}');
+          print('Weight: ${team.maxPokemon}');
+          print(
+              'Evolutions: ${team.pokemonTeam.map((e) => e.namePokemon).join(', ')}');
+          print(
+              'Evolutions: ${team.pokemonTeam.map((e) => e.attackTeam).join(', ')}');
+          print(
+              'Evolutions: ${team.pokemonTeam.map((e) => e.defenseTeam).join(', ')}');
+          print(
+              'Evolutions: ${team.pokemonTeam.map((e) => e.hpTeam).join(', ')}');
+          print(
+              'Evolutions: ${team.pokemonTeam.map((e) => e.numTeam).join(', ')}');
+        }
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error printing Pokemon data: $e');
+      }
+    }
+  }
+
   Future<void> fetchAndStoreTeam() async {
     //Es de prueba eliminar en produccion
     try {
@@ -108,33 +183,6 @@ class TeamService {
         print('Error fetching Pokemon data: $e');
       }
       throw Exception('Failed to fetch Team in getAllTeam()');
-    }
-  }
-
-  Future<void> printAllPokemon(List<Team> teamList) async {
-    try {
-      for (var team in teamList) {
-        if (kDebugMode) {
-          print('Name: ${team.nameTeam}');
-          print('Number: ${team.contPokemon}');
-          print('Height: ${team.descriptionTeam}');
-          print('Weight: ${team.maxPokemon}');
-          print(
-              'Evolutions: ${team.pokemonTeam.map((e) => e.namePokemon).join(', ')}');
-          print(
-              'Evolutions: ${team.pokemonTeam.map((e) => e.attackTeam).join(', ')}');
-          print(
-              'Evolutions: ${team.pokemonTeam.map((e) => e.defenseTeam).join(', ')}');
-          print(
-              'Evolutions: ${team.pokemonTeam.map((e) => e.hpTeam).join(', ')}');
-          print(
-              'Evolutions: ${team.pokemonTeam.map((e) => e.numTeam).join(', ')}');
-        }
-      }
-    } catch (e) {
-      if (kDebugMode) {
-        print('Error printing Pokemon data: $e');
-      }
     }
   }
 }
