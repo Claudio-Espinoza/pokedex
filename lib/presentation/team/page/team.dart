05a8aa22b247cb4a43351f1f26bb7e06f6d7b78f
Claudio Espinoza <claudioyugen@gmail.com>
2024-08-15 08:50:11 -0400

    refactor<team>: genera la lista con el local storage

--- lib/presentation/team/page/team.dart
+++ lib/presentation/team/page/team.dart
@@ -4,6 +4,7 @@ import 'package:pokedex_demo/core/configs/themes/app_colors.dart';
 import 'package:pokedex_demo/core/configs/themes/app_layer.dart';
 import 'package:pokedex_demo/presentation/landing/page/landing.dart';
 import 'package:pokedex_demo/presentation/team/page/team_form.dart';
+import 'package:pokedex_demo/presentation/team/widget/list_team.dart';
 
 class Team extends StatelessWidget {
   const Team({super.key});
@@ -51,11 +52,9 @@ class Team extends StatelessWidget {
                 ),
               ),
             ),
-            Flexible(
+            const Flexible(
               flex: 8,
-              child: Container(
-                color: AppColors.alertGreen,
-              ),
+              child: ListTeam(),
             )
           ],
         ),
--- lib/presentation/team/widget/list_team.dart
+++ lib/presentation/team/widget/list_team.dart
@@ -1,11 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:pokedex_demo/common/helper/util.dart';
+import 'package:pokedex_demo/common/widget/custom_buton.dart';
+import 'package:pokedex_demo/core/configs/themes/app_colors.dart';
+import 'package:pokedex_demo/core/configs/themes/app_layer.dart';
+import 'package:pokedex_demo/domain/model/team.dart';
+import 'package:pokedex_demo/domain/service/team_service.dart';
+import 'package:pokedex_demo/presentation/landing/page/landing.dart';
 
 class ListTeam extends HookWidget {
   const ListTeam({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Future<List<Team>> teams = TeamService().getAllTeam();
+
+    return FutureBuilder<List<Team>>(
+      future: teams,
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+          // Eliminar duplicados usando un Set
+          final uniqueTeams = snapshot.data!.toSet().toList();
+
+          return ListView.builder(
+            itemCount: uniqueTeams.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: AppLayer.marginHorizontal,
+                    vertical: AppLayer.marginHorizontal - 20),
+                child: CustomButton(
+                  text: uniqueTeams[index].nameTeam,
+                  onPressed: () =>
+                      Util.redirectToPage(context, const LandingPage()),
+                  backgroundColor: AppColors.red,
+                ),
+              );
+            },
+          );
+        } else {
+          return Center(child: Text('No teams available'));
+        }
+        // Add a return statement here
+      },
+    );
   }
 }
