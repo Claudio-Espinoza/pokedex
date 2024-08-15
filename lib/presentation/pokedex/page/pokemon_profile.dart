6081a03e84e2f0c122dc44561631823c0eb704a5
Daniel Alejandro <d.toro05@ufromail.cl>
2024-08-14 22:27:39 -0400

    Update <PokemonProfile>: Se agrego appbar e imagen del pokemon

--- lib/presentation/pokedex/page/pokemon_profile.dart
+++ lib/presentation/pokedex/page/pokemon_profile.dart
@@ -1,5 +1,7 @@
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:pokedex_demo/common/enum/type_colors.dart';
 import 'package:pokedex_demo/core/configs/themes/app_colors.dart';
 
 import '../../../domain/model/pokemon.dart';
@@ -11,7 +13,6 @@ class PokemonProfile extends StatefulWidget {
   });
 
   final Pokemon pokemon;
-
   @override
   _PokemonProfileState createState() => _PokemonProfileState();
 }
@@ -19,49 +20,92 @@ class PokemonProfile extends StatefulWidget {
 class _PokemonProfileState extends State<PokemonProfile> {
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
+    double _width = MediaQuery.of(context).size.width;
+    double _height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: AppColors.background,
       body: Stack(
         children: [
-          Positioned(
-            top: 0,
-            child: Container(
-              width: width,
-              color: Colors.green,
-              height: height * 0.45,
-            ),
+          
+          Container(
+            width: _width,
+            color: TypeColor.pokemonTypeColors[widget.pokemon.type[0]],
+            height: _height * 1.65,
           ),
           Positioned(
             bottom: 0,
             child: Container(
-              width: width,
-              height: height * 0.55,
-              color: Colors.red,
-              child: DefaultTabController(
-                length: 3,
-                child: Column(
-                  children: [
-                    TabBar(
-                      tabs: <Widget>[
-                        Tab(text: "Primera pagina"),
-                        Tab(text: "Segunda pagina"),
-                        Tab(text: "Tercera pagina"),
-                      ],
-                    ),
-                    Expanded(
-                      child: TabBarView(
-                        children: [
-                          Center(child: Text("Primera pagina")),
-                          Center(child: Text("Segunda pagina")),
-                          Center(child: Text("Tercera pagina")),
-                        ],
-                      ),
-                    ),
-                  ],
+              width: _width,
+              height: _height * 0.55,
+              decoration: BoxDecoration(
+                color: AppColors.background,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
                 ),
               ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                child: DefaultTabController(
+                  length: 3,
+                  child: Column(
+                    children: [
+                      TabBar(
+                        labelPadding: EdgeInsets.symmetric(horizontal: 10),
+                        labelStyle: TextStyle(fontSize: 12),
+                        unselectedLabelStyle: TextStyle(fontSize: 12),
+                        tabs: <Widget>[
+                          Tab(text: "Información"),
+                          Tab(text: "Estadística Básica"),
+                          Tab(text: "Evolución"),
+                        ],
+                      ),
+                      Expanded(
+                        child: TabBarView(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Center(child: Text("Primera página", textAlign: TextAlign.center)),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Center(child: Text("Segunda página", textAlign: TextAlign.center)),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Center(child: Text("Tercera página", textAlign: TextAlign.center)),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: _height * 0.2,
+
+            child: Container(
+              padding: EdgeInsets.all(0),
+              //color: Colors.black,
+              decoration: BoxDecoration(border: Border.all(color: Colors.black, strokeAlign: BorderSide.strokeAlignCenter)),
+              height: 250,
+              width: _width,
+              
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(height: 5,),
+                  Image.network(
+                    fit: BoxFit.contain,
+                    widget.pokemon.img),
+                ],
+              ),
+              
             ),
           ),
         ],
