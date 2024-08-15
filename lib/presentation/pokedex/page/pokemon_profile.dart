293825771c2021e27fec6701471e166c684a455d
Claudio Espinoza <claudioyugen@gmail.com>
2024-08-15 14:21:58 -0400

    refactor: cambio de nombramiento

--- lib/presentation/pokedex/page/pokemon_profile.dart
+++ lib/presentation/pokedex/page/pokemon_profile.dart
@@ -1,6 +1,4 @@
-
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/adapters.dart';
 import 'package:pokedex_demo/common/enum/type_colors.dart';
 import 'package:pokedex_demo/core/configs/themes/app_colors.dart';
 
@@ -20,32 +18,31 @@ class PokemonProfile extends StatefulWidget {
 class _PokemonProfileState extends State<PokemonProfile> {
   @override
   Widget build(BuildContext context) {
-    double _width = MediaQuery.of(context).size.width;
-    double _height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: AppColors.background,
       body: Stack(
         children: [
-          
           Container(
-            width: _width,
+            width: width,
             color: TypeColor.pokemonTypeColors[widget.pokemon.type[0]],
-            height: _height * 1.65,
+            height: height * 1.65,
           ),
           Positioned(
             bottom: 0,
             child: Container(
-              width: _width,
-              height: _height * 0.55,
-              decoration: BoxDecoration(
+              width: width,
+              height: height * 0.55,
+              decoration: const BoxDecoration(
                 color: AppColors.background,
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(30),
                   topRight: Radius.circular(30),
                 ),
               ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
                 child: DefaultTabController(
                   length: 3,
                   child: Column(
@@ -64,16 +61,22 @@ class _PokemonProfileState extends State<PokemonProfile> {
                         child: TabBarView(
                           children: [
                             Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Center(child: Text("Primera página", textAlign: TextAlign.center)),
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(
+                                  child: Text("Primera página",
+                                      textAlign: TextAlign.center)),
                             ),
                             Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Center(child: Text("Segunda página", textAlign: TextAlign.center)),
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(
+                                  child: Text("Segunda página",
+                                      textAlign: TextAlign.center)),
                             ),
                             Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Center(child: Text("Tercera página", textAlign: TextAlign.center)),
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(
+                                  child: Text("Tercera página",
+                                      textAlign: TextAlign.center)),
                             ),
                           ],
                         ),
@@ -85,27 +88,27 @@ class _PokemonProfileState extends State<PokemonProfile> {
             ),
           ),
           Positioned(
-            top: _height * 0.2,
-
+            top: height * 0.2,
             child: Container(
-              padding: EdgeInsets.all(0),
+              padding: const EdgeInsets.all(0),
               //color: Colors.black,
-              decoration: BoxDecoration(border: Border.all(color: Colors.black, strokeAlign: BorderSide.strokeAlignCenter)),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Colors.black,
+                      strokeAlign: BorderSide.strokeAlignCenter)),
               height: 250,
-              width: _width,
-              
+              width: width,
+
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Container(height: 5,),
-                  Image.network(
-                    fit: BoxFit.contain,
-                    widget.pokemon.img),
+                  Container(
+                    height: 5,
+                  ),
+                  Image.network(fit: BoxFit.contain, widget.pokemon.img),
                 ],
               ),
-              
             ),
           ),
         ],
