125b34fbefd1a9c73b643d5d26d6d805e460c0b7
Daniel Alejandro <d.toro05@ufromail.cl>
2024-08-27 12:22:25 -0400

    Add<PokemonProfile>: Se agregaron atributos

--- lib/core/configs/themes/app_themes.dart
+++ lib/core/configs/themes/app_themes.dart
@@ -3,6 +3,20 @@ import 'package:pokedex_demo/core/configs/themes/app_colors.dart';
 import 'package:pokedex_demo/core/configs/themes/app_layer.dart';
 
 class AppThemes {
+  static final profileLabelTextStyle = const TextStyle(
+    fontFamily: 'LeagueSpartan',
+    fontSize: 18,
+    fontWeight: FontWeight.w300,
+  );
+
+  static final profileFliedTextStyle = TextStyle(
+    fontFamily: 'LeagueSpartan',
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
+
+
+
   static final lightTheme = ThemeData(
       scaffoldBackgroundColor: AppColors.background,
       brightness: Brightness.light,
