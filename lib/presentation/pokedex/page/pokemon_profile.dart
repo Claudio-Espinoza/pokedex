b3b3c134bee0d2a41d31cd1b1b4cc0c971c24796
Daniel Alejandro <d.toro05@ufromail.cl>
2024-08-31 19:09:12 -0400

    Fix<PokemonProfile>: Correccion de codigo, reemplazo por widget

--- lib/presentation/pokedex/page/pokemon_profile.dart
+++ lib/presentation/pokedex/page/pokemon_profile.dart
@@ -8,6 +8,7 @@ import 'package:pokedex_demo/common/widget/pokemon_type.dart';
 import 'package:pokedex_demo/core/configs/themes/app_colors.dart';
 import 'package:pokedex_demo/core/configs/themes/app_themes.dart';
 import 'package:pokedex_demo/presentation/pokedex/page/pokedex.dart';
+import 'package:pokedex_demo/presentation/pokedex/widget/information_pokemon_profile.dart';
 
 import '../../../domain/model/pokemon.dart';
 
@@ -67,85 +68,7 @@ class _PokemonProfileState extends State<PokemonProfile> {
                       Expanded(
                         child: TabBarView(
                           children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-                              child: Column(
-                                children: [
-                                  Row(
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.symmetric(horizontal: 22,vertical: 10),
-                                        child: Column(
-                                          
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                            Text("Categoria",textAlign: TextAlign.left, style: AppThemes.profileLabelTextStyle),
-                                            Text("Altura",textAlign: TextAlign.left, style: AppThemes.profileLabelTextStyle),
-                                            Text("Peso",textAlign: TextAlign.left, style: AppThemes.profileLabelTextStyle),
-                                            Text("Habilidad",textAlign: TextAlign.left, style: AppThemes.profileLabelTextStyle),
-                                          ],
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.symmetric(horizontal: 40,vertical: 10),
-                                        child: Column(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                            Text(widget.pokemon.type[0], style: AppThemes.profileFliedTextStyle,),
-                                            Text(widget.pokemon.about.height, style: AppThemes.profileFliedTextStyle),
-                                            Text(widget.pokemon.about.weight, style: AppThemes.profileFliedTextStyle),
-                                            Text("obtenerhabilidad", style: AppThemes.profileFliedTextStyle)
-                                          ],
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(left: 22,bottom: 40),
-                                    child: Row(
-                                      
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      children: [
-                                        Column(
-                                        mainAxisAlignment: MainAxisAlignment.start,
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          const Text("Tipo",style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.w500
-                                          )),
-                                          Wrap(
-                                            spacing: 15,
-                                            children: widget.pokemon.type.map((String type){
-                                              return PokemonType(tipo: type);
-                                            }).toList()
-                                          ),
-                                          const SizedBox(height: 20, width: 20),
-                                          const Text("Debilidad",style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.w500
-                                          )),
-                                          SizedBox(
-                                            width: 300,
-                                            height: 100,
-                                            child: Wrap(
-                                              alignment: WrapAlignment.start,
-                                              spacing: 15,
-                                              runSpacing: 10,
-                                              children: widget.pokemon.about.weaknesses.map((String weaknesses){
-                                                return PokemonType(tipo: weaknesses);
-                                              }).toList()
-                                            ),
-                                          ),
-                                        ],
-                                        ),
-                                      ],
-                                    
-                                    ),
-                                  )
-],
-                              ),
-                            ),
+                            InformationPokemonProfile(pokemon: widget.pokemon),
                             const Padding(
                               padding: EdgeInsets.all(8.0),
                               child: Center(
@@ -214,3 +137,4 @@ class _PokemonProfileState extends State<PokemonProfile> {
     );
   }
 }
+
