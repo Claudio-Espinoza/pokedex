debe5fb44d839bbcbf7b3b7d689eb1359f367f3d
Daniel Alejandro <d.toro05@ufromail.cl>
2024-10-15 02:11:51 -0300

    Create<SearchInput>: Creacion de vista de busqueda de pokemon

--- lib/common/widget/search_bard.dart
+++ lib/common/widget/search_bard.dart
@@ -1,26 +1,154 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:pokedex_demo/common/enum/type_colors.dart';
+import 'package:pokedex_demo/common/helper/util.dart';
+import 'package:pokedex_demo/common/widget/custom_buton.dart';
 import 'package:pokedex_demo/core/configs/themes/app_colors.dart';
 import 'package:pokedex_demo/core/configs/themes/app_themes.dart';
+import 'package:pokedex_demo/domain/model/pokemon.dart';
+import 'package:pokedex_demo/domain/service/pokemon_service.dart';
+import 'package:pokedex_demo/presentation/pokedex/page/pokemon_profile.dart';
 
 class SearchInput extends StatelessWidget {
   final TextEditingController controller;
-  final String labelText;
+  final PokemonService pokemonService = PokemonService();
 
-  const SearchInput({
+  SearchInput({
     super.key,
     required this.controller,
-    this.labelText = 'Buscar Pokémon',
   });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
-      style: const TextStyle(color: AppColors.font),
+      style: const TextStyle(color: AppColors.font, fontSize: 18.0),
       decoration: AppThemes.inputTheme,
-      onSubmitted: (text) {
-        print('Input: $text'); // Aqui tendriamos agregarle la logica de buscar
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SearchResultsView(
+              controller: controller,
+              pokemonService: pokemonService,
+            ),
+          ),
+        );
       },
+      onSubmitted: (text) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SearchResultsView(
+              controller: controller,
+              pokemonService: pokemonService,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class SearchResultsView extends StatefulWidget {
+  final TextEditingController controller;
+  final PokemonService pokemonService;
+
+  const SearchResultsView({
+    super.key,
+    required this.controller,
+    required this.pokemonService,
+  });
+
+  @override
+  _SearchResultsViewState createState() => _SearchResultsViewState();
+}
+
+class _SearchResultsViewState extends State<SearchResultsView> {
+  List<Pokemon> allPokemon = [];
+  List<Pokemon> filteredPokemon = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPokemon();
+    widget.controller.addListener(_filterPokemon);
+  }
+
+  Future<void> _loadPokemon() async {
+    try {
+      List<Pokemon> pokemonList = await widget.pokemonService.getAllPokemon();
+      setState(() {
+        allPokemon = pokemonList;
+        filteredPokemon = pokemonList;
+      });
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error loading Pokémon: $e');
+      }
+    }
+  }
+
+  void _filterPokemon() {
+    setState(() {
+      filteredPokemon = allPokemon
+          .where((pokemon) =>
+              pokemon.name.toLowerCase().contains(widget.controller.text.toLowerCase()))
+          .toList();
+    });
+  }
+
+  @override
+  void dispose() {
+    widget.controller.removeListener(_filterPokemon);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 8, right: 8, top: 45, bottom: 16),
+            child: Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.arrow_back, color: AppColors.font),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                Expanded(
+                  child: SearchInput(controller: widget.controller),
+                ),
+              ],
+            ),
+          ),
+          
+          Expanded(
+            child: ListView.builder(
+              
+              itemCount: filteredPokemon.length,
+              itemBuilder: (context, index) {
+                final pokemon = filteredPokemon[index];
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0, left: 8, right: 8),
+                  child: CustomButton(
+                    text: "${pokemon.name}; ${pokemon.type.join(', ')}",
+                    onPressed: () => Util.redirectToPage(
+                      context,
+                      PokemonProfile(pokemon: pokemon),
+                    ),
+                    backgroundColor: TypeColor.pokemonTypeColors[
+                        pokemon.type[0].toString()],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
