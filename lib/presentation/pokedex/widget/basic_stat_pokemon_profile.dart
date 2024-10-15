c771569f08ea5a2383f504d0920e26d43e8385ba
Daniel Alejandro <d.toro05@ufromail.cl>
2024-10-14 23:47:16 -0300

    Create<PokemoStat>: Creacion de vista de estadistica basica de pokemon

--- lib/presentation/pokedex/widget/basic_stat_pokemon_profile.dart
+++ lib/presentation/pokedex/widget/basic_stat_pokemon_profile.dart
@@ -1,10 +1,9 @@
-
 import 'package:flutter/material.dart';
+import 'package:pokedex_demo/core/configs/themes/app_colors.dart';
+import 'package:pokedex_demo/core/configs/themes/app_themes.dart';
 import 'package:pokedex_demo/domain/model/pokemon.dart';
 
-class BasicStatPokemonProfile extends StatelessWidget{
-  
-
+class BasicStatPokemonProfile extends StatelessWidget {
   const BasicStatPokemonProfile({
     super.key,
     required this.pokemon,
@@ -12,21 +11,57 @@ class BasicStatPokemonProfile extends StatelessWidget{
 
   final Pokemon pokemon;
 
+  final int maxStatValue = 150;
+  final double maxWidth = 140;
 
-  
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        
+        statRow("HP", pokemon.stats.hp, maxStatValue, maxWidth),
+        statRow("Ataque", pokemon.stats.attack, maxStatValue, maxWidth),
+        statRow("Defensa", pokemon.stats.defense, maxStatValue, maxWidth),
+        statRow("Ataque especial", pokemon.stats.spAttack, maxStatValue, maxWidth),
+        statRow("Defensa especial", pokemon.stats.spDefense, maxStatValue, maxWidth),
+        statRow("Velocidad", pokemon.stats.speed, maxStatValue, maxWidth),
       ],
     );
-        
-      
-    
   }
 
+  Widget statRow(String statName, int statValue, int maxStatValue, double maxWidth) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0), // Espaciado entre filas
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(width: 130, child: Text(statName, style: AppThemes.profileLabelTextStyle)),  // Ajusta el ancho del texto
+          SizedBox(width: 40, child: Text(statValue.toString(), style: AppThemes.profileFliedTextStyle,)), // Espacio fijo para el valor del stat
+          statBar(statValue, maxStatValue, maxWidth),
+        ],
+      ),
+    );
+  }
+}
 
-  
+Widget statBar(int statValue, int maxStatValue, double maxWidth) {
+  double barWidth = (statValue / maxStatValue) * maxWidth;
 
+  return Stack(
+    alignment: Alignment.centerLeft,
+    children: [
+      Container(
+        height: 10,
+        width: maxWidth,
+        decoration: BoxDecoration(
+          color: AppColors.disableFont,
+          borderRadius: BorderRadius.circular(10)
+        )),
+      Container(
+        color: (statValue > 0.5 * maxStatValue) ? AppColors.alertGreen : AppColors.alerRed, // Color de la barra que representa el stat
+        height: 10,
+        width: barWidth,
+      ),
+    ],
+  );
 }
